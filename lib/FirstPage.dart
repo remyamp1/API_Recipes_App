8365c0de9d440b30d43e3a979274d70b592408b5
remyamp <remyamp774@gmail.com>
2024-10-23 13:19:45 +0530

    23-10-24

---
+++ lib/FirstPage.dart
@@ -0,0 +1,102 @@
+import 'dart:convert';
+
+
+import 'package:api_recipes_app/Data_Model.dart';
+import 'package:api_recipes_app/Data_Model.dart';
+import 'package:api_recipes_app/Recipes.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart'as http;
+class First extends StatefulWidget {
+  const First({super.key});
+
+  @override
+  State<First> createState() => _FirstState();
+}
+
+class _FirstState extends State<First> {
+  bool _isLoading=true;
+  @override
+  void initState(){
+    super.initState();
+    _getData();
+  }
+Recipesmodelapi? dataFromAPI;
+  _getData() async{
+    try {
+      String url="https://dummyjson.com/recipes";
+      http.Response res=await http.get(Uri.parse(url));
+      if(res.statusCode ==200){
+        dataFromAPI=Recipesmodelapi.fromJson(json.decode(res.body));
+        _isLoading=false;
+        setState(() {});
+      }
+    } catch (e) {
+     debugPrint (e.toString());
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       backgroundColor:  const Color.fromARGB(255, 255, 195, 232),
+      appBar: AppBar(
+        title: Text("Recipes App"),
+        backgroundColor: const Color.fromARGB(255, 255, 195, 232),
+      ),
+      body: _isLoading?
+      const Center(
+        child: CircularProgressIndicator(),
+      )
+      :dataFromAPI==null?
+      const Center(
+        child: Text('Failed to load data'),
+      )
+      
+     : GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,mainAxisSpacing: 2,crossAxisSpacing: 3),
+     itemCount: dataFromAPI!.recipes.length,
+       itemBuilder: (context,index){
+        final Recipe =dataFromAPI!.recipes[index];
+        return GestureDetector(
+          onTap: (){
+            Navigator.push(context,MaterialPageRoute(builder: (context)=>Recipes(recipes: Recipe,)));
+          },
+          child: Container(
+            height: 300,
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),
+            border: Border.all(color: Colors.black,
+            width: 2),
+            boxShadow: [
+              BoxShadow(
+                color: const Color.fromARGB(255, 231, 223, 223),
+                blurRadius: 0,
+                spreadRadius: 1,
+              )
+            ]),
+            margin: EdgeInsets.all(8.0),
+            padding: EdgeInsets.all(8.0),
+           child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                    
+                Image.network(Recipe.image,width: 100,height: 65,),
+            SizedBox(height: 10),
+                
+                Text(Recipe.name,style: TextStyle(fontSize: 10,color: Colors.black),),
+               SizedBox(height: 5),
+
+                Row(
+                  children: [
+                    SizedBox(width: 30),
+                    Text(Recipe.cuisine),
+                    SizedBox(width: 5),
+      
+           
+                  ],
+                )
+              ],
+            ),
+          ),
+        );
+      }),
+    );
+  }
+}
---
+++ lib/Recipes.dart
@@ -0,0 +1,50 @@
+
+import 'package:api_recipes_app/Data_Model.dart';
+import 'package:flutter/material.dart';
+
+class Recipes extends StatelessWidget{
+
+   final Recipe recipes;
+const Recipes({required this.recipes});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+   backgroundColor:  const Color.fromARGB(255, 255, 195, 232),
+   
+      appBar: AppBar(
+        
+        title: Text(recipes.name,style: TextStyle(fontSize: 20,color: Colors.black),),
+        backgroundColor: const Color.fromARGB(255, 255, 195, 232) ,
+      ),
+      body: 
+      Center(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: SingleChildScrollView(
+            child: Column(
+              
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              
+              children: [
+                Center(child: Image.network(recipes.image,width: 350,height: 300,)),
+                SizedBox(height: 50),
+               Text("Name:${recipes.name}",style: TextStyle(fontSize: 20,color: Colors.black),),
+              SizedBox(height: 15),
+               Text("Cuisine:${recipes.cuisine}",style: TextStyle(fontSize: 15,color: Colors.black),),
+                
+              SizedBox(height: 20),
+                Text("Ingredients:${recipes.ingredients}",style: TextStyle(fontSize: 15,),textAlign: TextAlign.justify,),
+                 SizedBox(height: 20),
+                  Text('Instrucations:${recipes.instructions}',style: TextStyle(fontSize: 15,),textAlign: TextAlign.justify,),
+                  SizedBox(height: 20),
+                  Text('Cooking Time:${recipes.cookTimeMinutes}',style: TextStyle(fontSize: 15,color: Colors.black),)
+              ],
+            ),
+          ),
+        ),
+      )
+       
+    );
+  }
+}
